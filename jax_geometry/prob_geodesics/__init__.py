7b75f46e4ddf8f75c78b0998eafd91d480cec914
Frederik Mbius Rygaard <fmry@pop-os.localdomain>
2025-04-12 15:10:29 +0200

    Update

---
+++ jax_geometry/prob_geodesics/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 16:13:51 2025
+
+@author: fmry
+"""
+
+#%% Modules
+
+from .prob_georce import ProbGEORCE, ProbGEORCE_Embedded, ProbGEORCE_Euclidean
